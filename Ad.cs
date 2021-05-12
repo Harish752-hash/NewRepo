58b1afccd6b60de33bec04c625365f7a5b497802
Harishreddy veldanda <Harishreddy veldanda@DESKTOP-3N16O8P>
2021-05-12 16:23:40 +0530

    Add project files.

---
+++ Ad.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Addition
+{
+    class Ad
+    {
+        int x, y;
+        public Ad(int i,int j)
+        {
+            x = i;
+            y = j;
+        }
+        public int Addition(Ad e)
+        {
+            int z = e.x + e.y;
+            return z;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Addition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ad ad = new Ad(89, 56);
+            int x = ad.Addition(ad);
+            Console.WriteLine(x);
+        }
+    }
+}
